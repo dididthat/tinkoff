11ad522c156da0e71b1a3df7567f052d5f0ffc65
dididthat <darianikulina02@gmail.com>
2023-04-29 15:31:58 +0300

    begin

--- AmazingApp/ViewController.swift
+++ AmazingApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        view.backgroundColor = .green
+        print(#function)
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#function)
+    }
+  
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
+    }
+    override func viewWillLayoutSubviews() {
+        print(#function)
+        super.viewWillLayoutSubviews()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print(#function)
+    }
+    
 
 
 }
-
+//MARK:
