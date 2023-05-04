e7e9701388321b7c95310d1ab7538c317d5f972e
dididthat <darianikulina02@gmail.com>
2023-05-04 17:33:32 +0300

    dz2_finish

--- AmazingApp/ProfileViewController.swift
+++ AmazingApp/ProfileViewController.swift
@@ -15,10 +15,9 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var profileDescription: UITextField!
     @IBOutlet weak var profileEdit: UIButton!
+    let profileAdd = UIButton()
     
-    @IBOutlet weak var profileAdd: UIButton!
-    
-    
+// так как init запускается раньше, то frame он не может найти, т.к. он находится во viewDidLoad
 //    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
 //        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 //        print(profileAdd.frame)
@@ -32,55 +31,43 @@ class ProfileViewController: UIViewController {
     @IBAction func Edit(_ sender: Any) {
     }
     
-    @IBAction func profileAddPhoto(_ sender: Any) {
-        let controller = UIAlertController(title: "hey", message: "tata", preferredStyle: .actionSheet)
-        present(controller, animated: true)
-        
-        print("выбери изображение профиля")
-    }
-
-//    let cameraImageView: UIImageView = {
-//        let imageView = UIImageView()
-//        imageView.image = UIImage(systemName: "camera")
-//
-//        imageView.backgroundColor = UIColor(hex6: 0x3F78F0)
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//       return imageView
-//    }()
-
-
     override func viewDidLoad() {
         
         super.viewDidLoad()
         view.backgroundColor = .white
         print(#function)
-        let largeConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .bold, scale: .large)
-               
-        let largeBoldDoc = UIImage(systemName: "camera", withConfiguration: largeConfig)
-        profileAdd.setTitle("", for: .normal)
-        
-        profileAdd.setImage(largeBoldDoc, for: .normal)
         profilePhoto.layer.cornerRadius = 40
- 
-        profileAdd.backgroundColor = UIColor(hex6: 0x3F78F0)
+        
         profileAdd.layer.cornerRadius = 40
         profileAdd.layer.masksToBounds = false
         profileEdit.layer.cornerRadius = 5
         profileEdit.layer.borderWidth = 1
         profileEdit.layer.borderColor = UIColor.black.cgColor
         profilePhoto.layer.masksToBounds = true
-        
-//        profileAdd.addSubview(cameraImageView)
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .bold, scale: .large)
+        let largeBoldDoc = UIImage(systemName: "camera", withConfiguration: largeConfig)?.withRenderingMode(.alwaysOriginal).withTintColor(.white)
+        profileAdd.setTitle("", for: .normal)
+        profileAdd.backgroundColor = UIColor(hex6: 0x3F78F0)
+        profileAdd.setImage(largeBoldDoc, for: .normal)
+        profileAdd.addTarget(self, action: #selector(profileAddAction), for: .touchUpInside)
+        setUp()
+    }
+    @objc
+    func profileAddAction() {
+        print("Кнопка нажата")
+    }
+    
+    private func setUp() {
+        profileAdd.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileAdd)
+        NSLayoutConstraint.activate ([
+            profileAdd.heightAnchor.constraint(equalToConstant: 80),
+            profileAdd.trailingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 0),
+            profileAdd.bottomAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 0)
 
-
-//        NSLayoutConstraint.activate ([
-//            cameraImageView.centerXAnchor.constraint(equalTo: profileAdd.centerXAnchor),
-//            cameraImageView.centerYAnchor.constraint(equalTo: profileAdd.centerYAnchor)
-//
-//        ])
+        ])
 
     }
-
     
     
 
